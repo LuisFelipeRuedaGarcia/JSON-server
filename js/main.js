64f668bfb6371430379da8853f4a475e0460eaeb
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-05-04 17:54:59 -0500

    11

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 console.log("conectado");
-const url = "http://localhost:4002/camper"
+const url = "http://localhost:3000/camper"
 let Post = async (object) => {
     console.log(object);
-    try{
-    await fetch(url,{method: "POST",
-    body: JSON.stringify(object),
-    headers:{
-        "Content-Type": "application/json"
-    }
-})
-    }catch (error) {
+    try {
+        await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(object),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    } catch (error) {
         console.log(error)
     }
     console.log("funcinando")
 }
 
-Post({nombre:"Sherman"});
+Post({ nombre: "Sherman" });
 
 const getCampers = async () => {
     try {
